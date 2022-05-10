1ac6a77801289f508fc28363bc56620e136eec3a
Felipe Yuri Silva <felipe.yuri@sankhya.com.br>
2022-05-10 00:38:24 -0300

    Finalizando botões de enviar feedback

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import { CloseButton } from "../../CloseButton/CloseButton"
 import { FeedbackType, feedbackTypes } from "../WidgetForm"
 
@@ -28,8 +28,27 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
 
             <form className="my-4 w-full">
                 <textarea
-                    className="min-w-[384px] w-full min-h-[112px] text-sm"
+                    className="min-w-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 
+                    border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 
+                    focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..." />
+                <footer className="flex gap-2 mt=2">
+                    <button
+                        type="button"
+                        className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 
+                        focus:ring-offset-zinc-900 focus:ring-brand-500"
+                    >
+                        <Camera className="w-6 h-6 text-zinc-100" />
+
+                    </button>
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparet flex-1 flex justify-center items-center text-sm 
+                        hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"   
+                    >
+                        Enviar feedback
+                    </button>
+                </footer>
             </form>
         </>
     )
